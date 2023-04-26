fc69b3ddc7e636129108186fb9f89b089aaff01c
Dave Bulaquena <davecharm16@gmail.com>
2023-04-27 05:10:05 +0800

    Created the Update Function

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Create from './pages/Create';
+import Update from './pages/Update';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/create' element={<Create/>}/>
+          <Route path='/update/:id' element={<Update/>}/>
         </Routes>
       </Router>
     </div>
